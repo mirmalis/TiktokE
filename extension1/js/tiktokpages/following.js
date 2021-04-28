d5a0c255bef817a439083fd12f4a1859cbed7ecb
mirmalis <10066881+mirmalis@users.noreply.github.com>
2021-04-28 16:59:03 +0300

    Changed port

--- extension1/js/tiktokpages/following.js
+++ extension1/js/tiktokpages/following.js
@@ -1,1 +1,1 @@
-
+console.log('following.js');
--- extension1/js/tiktokpages/foryou.js
+++ extension1/js/tiktokpages/foryou.js
@@ -1,1 +1,7 @@
 console.log('Foryou');
+async function dodo() {
+  if (document.querySelector(".trending-container") === null)
+    return;
+  console.log("is trending");
+}
+dodo();
